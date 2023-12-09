6a0f65c9b77a8cf2314df9b505952ffaa596af30
jfranco2 <jhoan.franco@detektor.com.co>
2023-12-09 12:30:10 -0500

    init: prueba despliegue con vite

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hola Mundo!!</h1>
+  
+`
+
+setupCounter(document.querySelector('#counter'))
